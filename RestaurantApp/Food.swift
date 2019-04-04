c7fcb9442d89259655da15a5c09a4351685c2a4d
ema2497 <49290161+ema2497@users.noreply.github.com>
2019-04-04 14:31:34 -0600

    Add files via upload

---
+++ RestaurantApp/Food.swift
@@ -0,0 +1,20 @@
+//
+//  Food.swift
+//  RestaurantApp
+//
+//  Created by macSO-TI on 15/03/19.
+//  Copyright © 2019 mane. All rights reserved.
+//
+
+import UIKit
+
+class Food{
+    var name:String?
+    var des:String?
+    var image:String?
+    init(name:String,des:String,image:String) {
+        self.name = name
+        self.des = des
+        self.image = image
+    }
+}
